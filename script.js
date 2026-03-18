59b258a8a308e8e7aa69ff6b78a75e6d486c3953
veres.csanad@ckik.hu <veres.csanad@ckik.hu>
2026-03-18 07:28:38 +0100

    Csgo lekeres fetch

---
+++ script.js
@@ -0,0 +1,6 @@
+const url='https:https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api/en/all.json';
+
+async function GetCSGOData() {
+    const csgo=await fetch(url);
+    const skin=await 
+}
