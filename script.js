6c0de7d755d15a2dc4787873f28e53c3a6dd1094
veres.csanad@ckik.hu <veres.csanad@ckik.hu>
2026-03-20 09:36:09 +0100

    javascript almost done and css is done i think so far

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const url='https:https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api/en/all.json';
+const url = 'https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api/en/skins_grouped.json';
 
 async function GetCSGOData() {
-    const csgo=await fetch(url);
-    const skin=await ;
+    const csgo = await fetch(url);
+    const skins = await csgo.json();
+    return skins;
+}
+
+function GetRandomSkin(skins) {
+    const randomindex = Math.floor(Math.random() * skins.length)
+    return skins[randomindex];
+}
+
+function ShowSkin(skin) {
+    document.getElementById("SkinName").innerText = skin.name;
+    document.getElementById("SkinRarity").innerText = `Ritkaság: ${skin.rarity.name}`;
+    document.getElementById("SkinImg").src = skin.image;
+    let button = document.getElementById("GenerateButton");
+    button.addEventListener("click", async () => {
+        const skins = await GetCSGOData();
+        const randomSkin = GetRandomSkin(skins);
+        ShowSkin(randomSkin);
+    });
 }
